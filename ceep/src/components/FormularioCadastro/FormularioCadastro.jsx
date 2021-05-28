6557d144d0c4805132e2b4b1f581ec617bce0242
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2021-05-28 14:52:30 +0000

    Adicionado eventos ao campos do formulário

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,14 +2,37 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  handleMudancaTitulo(event) {
+    this.titulo = event.target.value;
+  }
+
+  handleMudancaTexto(event) {
+    this.texto = event.target.value;
+  }
+
   render() {
     return (
       <form className="formulario-cadastro">
         <p>
-          <input type="text" placeholder="Título" />
+          <input
+            type="text"
+            placeholder="Título"
+            onChange={this.handleMudancaTitulo.bind(this)}
+          />
         </p>
         <p>
-          <textarea cols="100" rows="5" placeholder="Escreva sua nota" />
+          <textarea
+            cols="100"
+            rows="5"
+            placeholder="Escreva sua nota"
+            onChange={this.handleMudancaTexto.bind(this)}
+          />
         </p>
         <p>
           <button>Criar Nota</button>
