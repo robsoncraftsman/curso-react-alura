6b407ffdb8dde46a20e4869c633d267a0c4df304
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2021-07-21 16:43:29 -0300

    Versão inicial do projeto do curso de function components com React

---
+++ function-components/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/formularioCadastro/FormularioCadastro";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulário de Cadastro</h1>
+      <FormularioCadastro />
+    </Fragment>
+  );
+}
+
+export default App;
