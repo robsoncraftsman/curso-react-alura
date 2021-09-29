37bf9de6f40eef4b9e374a1a9ce4f5d35552d19e
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2021-09-29 17:19:56 +0000

    Adicionado estado ao formulário via react hooks

--- function-components/src/components/formularioCadastro/FormularioCadastro.jsx
+++ function-components/src/components/formularioCadastro/FormularioCadastro.jsx
@@ -1,39 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
-  let nome = "";
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
 
   return (
     <form
       onSubmit={(event) => {
-        console.log(nome);
+        console.log(nome, sobrenome, cpf);
         event.preventDefault();
       }}
     >
       <TextField
         id="nome"
         label="Nome:"
+        value={nome}
         variant="outlined"
         fullWidth={true}
         margin="normal"
         onChange={(event) => {
-          nome = event.target.value;
+          setNome(event.target.value);
         }}
       />
       <TextField
         id="sobrenome"
         label="Sobrenome:"
+        value={sobrenome}
         variant="outlined"
         fullWidth={true}
         margin="normal"
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
       />
       <TextField
         id="cpf"
         label="CPF:"
+        value={cpf}
         variant="outlined"
         fullWidth={true}
         margin="normal"
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
       />
 
       <FormControlLabel
